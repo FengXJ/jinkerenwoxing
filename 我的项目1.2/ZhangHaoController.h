65eb699dea7286da42d3db1816f86fade14cd24d
冯学杰 <578719825@qq.com>
2016-01-08 17:29:01 +0800

    SAVE UI

---
+++ 我的项目1.2/ZhangHaoController.h
@@ -0,0 +1,13 @@
+//
+//  ZhangHaoController.h
+//  我的项目1.2
+//
+//  Created by 冯学杰 on 16/1/8.
+//  Copyright © 2016年 冯学杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZhangHaoController : UIViewController
+
+@end
---
+++ 我的项目1.2/ZhangHaoController.m
@@ -0,0 +1,37 @@
+//
+//  ZhangHaoController.m
+//  我的项目1.2
+//
+//  Created by 冯学杰 on 16/1/8.
+//  Copyright © 2016年 冯学杰. All rights reserved.
+//
+
+#import "ZhangHaoController.h"
+
+@interface ZhangHaoController ()
+
+@end
+
+@implementation ZhangHaoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
